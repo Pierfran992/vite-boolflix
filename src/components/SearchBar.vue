51b22471930faf9cc5f82489754c4d672d59d3ff
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-16 15:09:07 +0100

    Creato il componente per cercare i film atrraverso il nome;

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,26 @@
+<!-- creao la barra di ricerca per cercare i film -->
 <script>
+import { store } from '../store.js';
+
 export default {
     name: "SearchBAr",
+    data() {
+        return {
+            store,
+        }
+    },
 }
 </script>
 
 <template>
-
+    <div class="container">
+        <input id="searchFilm" type="text" placeholder="Search film..." v-model="store.searchTitle">
+        <button type="submit" @click.prevent="$emit('search')">cercaaaaaa</button>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+#searchFilm {
+    margin: 50px 0;
+}
 </style>
---
+++ src/components/film-list-components/CardGenerator.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: "CardGenerator",
+
+    props: ["info", "urlImg"],
+}
+</script>
+
+<template>
+    <!-- creo la card -->
+    <div class="gen_card">
+        <div class="slot_img_card">
+            <img :src="urlImg + info.poster_path" :alt="info.title">
+        </div>
+        <h4>{{ info.title }}</h4>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.gen_card {
+    width: calc(100% / 5);
+    padding: 10px;
+    text-align: center;
+
+    * {
+        margin-bottom: 10px;
+    }
+}
+</style>
