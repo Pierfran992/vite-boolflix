385ee25d794b6ccbeb337184ecf8a6b21e2df43b
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-16 14:16:10 +0100

    Inserite nello store.js nuovi parametri: - url base da utilizzare; -url da utilizzare nella ricerca; - sostituito l'apiParameter con query;

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     filmList: [],
-    apiURL: "https://api.themoviedb.org/3/movie/popular?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT",
+    apiPopularURL: "https://api.themoviedb.org/3/movie/popular?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT",
+    apiSearchURL: "https://api.themoviedb.org/3/search/movie?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT&",
     searchTitle: "",
-    apiParameter: "title",
+    apiParameter: "query",
 });
