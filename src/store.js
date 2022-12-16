ec8c108a682c34093979db451fdc251b59f54a85
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-16 15:07:02 +0100

    Aggiunto il parametro apiImgUrl;

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
     filmList: [],
     apiPopularURL: "https://api.themoviedb.org/3/movie/popular?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT",
     apiSearchURL: "https://api.themoviedb.org/3/search/movie?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT&",
+    apiImgUrl: "https://image.tmdb.org/t/p/w342",
     searchTitle: "",
     apiParameter: "query",
 });
