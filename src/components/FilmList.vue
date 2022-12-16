3b15023a6de3bf97de13b82fdcdc9d5f923f6013
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-16 15:08:29 +0100

    Creato il componente in cui andranno inserite le card dei film ricercati;

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,13 +1,36 @@
+<!-- creao il componente in cui verrano stampati i risultati della ricerca -->
 <script>
+import CardGenerator from './film-list-components/CardGenerator.vue';
+
+import { store } from '../store.js';
+
 export default {
     name: "FilmList",
+    components: {
+        CardGenerator
+    },
+    data() {
+        return {
+            store,
+        }
+    },
 }
 </script>
 
 <template>
-
+    <div class="container">
+        <!-- creato un ciclo for sul componente per far stampare in FilmList tutti gli oggetti contenuti nell'array filmList -->
+        <CardGenerator v-for="obj in store.filmList" :key="obj.id" :info="obj" :urlImg="store.apiImgUrl" />
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
 
+.container {
+    @include between();
+    flex-wrap: wrap;
+    align-items: stretch;
+}
 </style>
