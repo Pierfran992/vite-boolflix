36bd25bb894cb5add19f3ee55a420cb7712443f1
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-16 14:17:07 +0100

    Creata la funzione per inserire i risultati dei film nell'array vuoto filmList;

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,30 @@ export default {
             store,
         }
     },
+    methods: {
+        // creo il metodo per far stampare dei risultati di film in pagina
+        getFilm() {
+            // innanzitutto associo a myUrl l'url di film popolari 
+            let myUrl = store.apiPopularURL;
+            // creata la condizione per far ricercare un film all'utente in base al nome che inserisce
+            if (store.searchTitle !== "") {
+                myUrl = `${store.apiSearchURL}${apiParameter}=${searchTitle}`;
+            }
+
+            axios
+                .get(myUrl)
+                .then(res => {
+                    store.filmList = res.data.results;
+                })
+                .catch(problem => {
+                    console.log("Errori", problem);
+                });
+        }
+    },
+    mounted() {
+        // faccio partire il metodo dopo che ha caricato la struttura della pagina
+        this.getFilm();
+    }
 }
 </script>
 
