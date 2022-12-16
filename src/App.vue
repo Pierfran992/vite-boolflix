3a71a5e8dd39c49c52d152b0333f9dcabf658930
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-16 13:11:20 +0100

    Creati i due componenti principali: -filmList in cui verranno mostrati i film ricercati; -searchBar con cui cercare i film;

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
+import FilmList from './components/FilmList.vue';
 
+import { store } from './store.js';
+
+export default {
+    name: "App",
+    components: {
+        SearchBar,
+        FilmList,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+}
 </script>
 
 <template>
+    <!-- componenti dell'header -->
+    <header>
+
+    </header>
+    <!-- componenti del main -->
+    <main>
+        <SearchBar />
+        <FilmList />
+    </main>
 
 </template>
 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "SearchBAr",
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
