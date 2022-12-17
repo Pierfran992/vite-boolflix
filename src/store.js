425a809e069eb8ee05269652c82e5640107a1d51
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-17 16:15:39 +0100

    Fix: - modificato apiSearchUrl;

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue';
 export const store = reactive({
     filmList: [],
     apiPopularURL: "https://api.themoviedb.org/3/movie/popular?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT",
-    apiSearchURL: "https://api.themoviedb.org/3/search/movie?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT&",
+    apiSearchURL: "https://api.themoviedb.org/3/search/multi?api_key=7947fac27724a3f0b35b77bc5930097d&language=it-IT&",
     apiImgUrl: "https://image.tmdb.org/t/p/w342",
     searchTitle: "",
     apiParameter: "query",
