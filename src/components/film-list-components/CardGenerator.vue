ed04fe01dc0809023558cc7a9a31d7a07c6b8cc1
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-17 16:17:13 +0100

    Sviluppo: -Stampate a schermo le info sull'oggetto richieste dal progetto;

--- src/components/film-list-components/CardGenerator.vue
+++ src/components/film-list-components/CardGenerator.vue
@@ -10,20 +10,24 @@ export default {
     <!-- creo la card -->
     <div class="gen_card">
         <div class="slot_img_card">
-            <img :src="urlImg + info.poster_path" :alt="info.title">
+            <img :src="urlImg + info.poster_path" :alt="info.title || info.name">
         </div>
-        <h4>{{ info.title }}</h4>
+        <h4>{{ info.title }} {{ info.name }}</h4>
+        <span>Titolo Originale: {{ info.original_title }} {{ info.original_name }}</span>
+
+        <span>Lingua Origanale: {{ info.original_language }}</span>
+
+        <span>Voto: {{ info.vote_average }}</span>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/partials/variables.scss' as *;
+@use '../../styles/partials/mixins.scss' as *;
+
 .gen_card {
     width: calc(100% / 5);
     padding: 10px;
-    text-align: center;
-
-    * {
-        margin-bottom: 10px;
-    }
+    @include column();
 }
 </style>
